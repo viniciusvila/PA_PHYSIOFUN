2e2c96f6423858fbfc09ff70f12a90a0b56659b3
viniciusvila <vinicius_vila@yahoo.com.br>
2017-05-14 23:14:39 -0300

    Upload script GiraTurreta
    Upload script GiraTurreta

--- 3 - Desenvolvimento Unity/PhysioFun/Assets/Scripts/GiraTurreta.cs
+++ 3 - Desenvolvimento Unity/PhysioFun/Assets/Scripts/GiraTurreta.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 
 public class GiraTurreta : MonoBehaviour {
 
-	public int ladoturreta;
 	public double maxesquerda;
 	public double maxdireita;
 	public int velocidade ;
-	private int girar=1;
+	public int vaiDireita = 1;
+	public int vaiEsquerda = 0;
 	// Use this for initialization
 
 	void Start () {
@@ -19,6 +19,14 @@ public class GiraTurreta : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
+			girarturreta ();
+
+
+	}
+
+	void girarturreta () {
 		// Rotate the object around its local X axis at 1 degree per second
 		//UP DOWN
 		//transform.Rotate(Vector3.right * Time.deltaTime);
@@ -26,22 +34,33 @@ public class GiraTurreta : MonoBehaviour {
 		// ...also rotate around the World's Y axis
 		//transform.Rotate(Vector3.up * Time.deltaTime * velocidade, Space.World);
 
-		if ((transform.rotation.eulerAngles.z > maxdireita) && (ladoturreta == 0) && (girar == 1)) {
-
+		/*if( (transform.rotation.eulerAngles.z < 180) && (transform.rotation.eulerAngles.z >= maxesquerda) ){
 			transform.Rotate ((Vector3.up * Time.deltaTime * velocidade) * -1, Space.World);
-
-			if (transform.rotation.eulerAngles.z > maxdireita) {
-				girar = 0;
-			}
-		}
-		/*	else if ( (transform.rotation.eulerAngles.z > maxdireita) && (ladoturreta ==0) ){
-			transform.Rotate(Vector3.up * Time.deltaTime * velocidade, Space.World);
-		
+		}else 
+			if ( (transform.rotation.eulerAngles.z > 180) && (transform.rotation.eulerAngles.z <= maxdireita) ){
+			transform.Rotate ((Vector3.up * Time.deltaTime * velocidade) * 1, Space.World);
 		}*/
 
 
-		if( (transform.rotation.eulerAngles.z > maxdireita) && (ladoturreta ==1) ){
-			transform.Rotate(Vector3.up * Time.deltaTime * velocidade, Space.World);
+
+		if ((vaiEsquerda == 1) && (vaiDireita == 0)) {
+			transform.Rotate ((Vector3.up * Time.deltaTime * velocidade) * -1, Space.World);
+			Debug.Log ("ESQUERDA " + transform.rotation.eulerAngles.y);
+		} else if ((vaiDireita == 1) && (vaiEsquerda == 0)) {
+			transform.Rotate ((Vector3.up * Time.deltaTime * velocidade) * 1, Space.World);
+			Debug.Log ("DIREITA " + transform.rotation.eulerAngles.y);
+		} 
+
+
+		if (transform.rotation.eulerAngles.y < maxesquerda) {
+			vaiDireita = 1;
+			vaiEsquerda = 0;
+			Debug.Log ("DIREITA1 " + transform.rotation.eulerAngles.y);
+		} else if (transform.rotation.eulerAngles.y > maxdireita) {
+			vaiDireita = 0;
+			vaiEsquerda = 1;
+			Debug.Log ("ESQUERDA1 " + transform.rotation.eulerAngles.y);
 		}
 	}
+
 }
